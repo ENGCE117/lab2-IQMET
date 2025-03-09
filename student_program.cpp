458fe89a5f302a942e57116ede332778e16586d8
67543206073-8 <62295657+IQMET@users.noreply.github.com>
2025-03-09 17:09:31 +0700

    Lab2.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,48 @@
 #include <stdio.h>
 
+char* reverse( char str1[] ) ;
+int my_strlen( char str[] ) ;
+
 int main() {
-    //YOUR CODE HERE
+
+    char text[ 50 ] ;
+    char *output ;
+
+    printf( "Enter str1 : " ) ;
+    gets( text ) ;
+
+    output = reverse( text ) ;
+
+    printf( "MAIN : %s\n" , output ) ;
+
     return 0 ;
-}//end function
+}
+
+char* reverse( char str1[] ) {
+
+    int x , i ;
+    char y ;
+
+    x = my_strlen( str1 ) ;
+
+    for ( i = 0 ; i < x / 2 ; i++ ) {
+        y = str1[ i ] ;
+        str1[ i ] = str1[ x - i - 1 ] ;
+        str1[ x - i - 1 ] = y ;
+    }
+
+    printf( "FUNC : %s\n" , str1 ) ;
+
+    return str1 ;
+}
+
+int my_strlen( char str[] ) {
+
+    int count = 0 ;
+
+    while ( str[ count ] != '\0' ) {
+        count++ ;
+    }
+
+    return count ;
+}
